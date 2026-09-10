5c69c1d1551fafe0a89bbca2aa6366a90c00d5f4
paulo870 <paulo@poliglotaangola.com>
2026-09-10 10:45:27 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -526,6 +526,7 @@ else {
             audioPlayer.load();
             showAudioPlayer(); // <-- use the new function
             audioPlayer.controls = true;
+            audioPlayer.setAttribute("controlsList", "nodownload");
         });
 
         audioListContainer.appendChild(btn);
